9963fd987697ab90bda414ffaeed599ff8834117
Stephanie <stephaniiehallberg@gmail.com>
2023-02-21 14:17:04 +0100

    feat(multiple-files): scoreCheck using struct Score

--- main.c
+++ main.c
@@ -68,63 +68,6 @@ void viewLowScoreboard()
     fclose(fdata.file_ptr);
 };
 
-// Function: scoreCheck
-// Description: Checks if score is highscore
-// Parameters: int tries
-// Returns: struct Score
-// Side-effects: None
-// Error-handling: None
-// Status: 'Working'
-Score scoreCheck(int tries)
-{
-    // Create score struct
-    Score score;
-
-    // Read file
-    FileData fdata = readFile();
-
-    // Set score
-    score.isHighScore = false; // start as highscore false
-
-    // Set rowline
-    int row_line = 1;
-
-    // variables
-    int number;
-    char line[256];
-
-    // loop through file
-    while (fgets(fdata.file_name, sizeof(line), fdata.file_ptr) != NULL)
-    {
-        // get score from string
-        sscanf(fdata.file_name, "%*s %*s %*s %d", &number);
-
-        // if number is higher than tries, highscore found
-        if (number > tries)
-        {
-            // High score found! create a new score.
-            score = scoreAdd(tries, row_line);
-            break;
-        }
-        // add to rowline
-        row_line++;
-    }
-
-    // count lines in file. If less than 6, add to last row
-    if (row_line < 6 && score.isHighScore == false)
-    {
-        // create a new score
-        score = scoreAdd(tries, row_line);
-    }
-
-
-    // close file
-    fclose(fdata.file_ptr);
-
-    // return score
-    return score;
-}
-
 // Function: playGame
 // Description: Plays a game of 'Guess the number'
 // Parameters: None
--- scoreinput.h
+++ scoreinput.h
@@ -4,3 +4,4 @@
 #include "Score.h"
 
 Score scoreAdd(int tries, int row);
+Score scoreCheck(int tries);
