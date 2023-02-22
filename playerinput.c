066fe4e9873421cb7d19d0729c66dfe88151568f
Stephanie <stephaniiehallberg@gmail.com>
2023-02-22 11:54:39 +0100

    feat(playerinput): playerAdd validate before input

--- playerinput.c
+++ playerinput.c
@@ -12,12 +12,21 @@ Player playerAdd(int points)
 {
     // string
     char *strTypeName = "Ange ditt namn: ";
+
     // Create player struct
     Player player;
 
     // set name by user input
     printf(strTypeName);
-    scanf("%s", player.name);
+
+    // Ensure prompt is displayed before input is read
+    fflush(stdout);
+
+    // Get name from user input
+    fgets(player.name, sizeof(player.name), stdin);
+
+    // Remove newline character from input
+    player.name[strcspn(player.name, "\n")] = '\0';
 
     // Get current date
     strcpy(player.date, getCurrentDate());
