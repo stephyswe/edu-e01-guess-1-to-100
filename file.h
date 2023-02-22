6d17cadef507e0290e296bc41438ef2b201ff29b
Stephanie <stephaniiehallberg@gmail.com>
2023-02-22 02:15:22 +0100

    feat(file): remove checkFileExist

--- file.h
+++ file.h
@@ -5,11 +5,6 @@
 #include "FileData.h"
 #include "Player.h"
 
-// Function: checkFileExist
-// Description: Checks if the file exists
-// Parameters: FILE *file_ptr
-void checkFileExist(FILE *file_ptr);
-
 // Function: readFile
 // Description: Reads the file and returns the data
 // Returns: FileData file_data
