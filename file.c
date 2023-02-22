2cdf36dfb9e70022a96d51ff74d1b8779b3fbf30
Stephanie <stephaniiehallberg@gmail.com>
2023-02-22 12:52:15 +0100

    feat(file): scoreToFile with while loop & better syntax

--- file.c
+++ file.c
@@ -77,57 +77,44 @@ void scoreToFile(int write_line, Player player)
     // newline will store the new line of text to be written to the file
     char newline[MAX_LINE];
 
-    // we'll keep reading the file so long as keep_reading is true
-    bool keep_reading = true;
-
-    // will keep track of the current line number we are reading from the file
-    int current_line = 1;
-
     // cast integer player_value to string newline
     // ensures that the destination buffer is not overflowed
     snprintf(newline, MAX_LINE, "%s %s %d", player.date, player.name, player.points);
 
-    // time_t - use a unique temporary file name for each run, for example by using a timestamp
     // snprintf will write the string "temp____" followed by the filename to
     // ensures that the destination buffer is not overflowed
-    time_t t = time(NULL);
-    snprintf(temp_filename, FILENAME_SIZE, "temp_%ld.txt", t);
-
-    // flush stdin to get the \n char from the last scanf out, otherwise the
-    // below fgets will 'fail' as it will immediately encounter a newline
-    fflush(stdin);
+    snprintf(temp_filename, FILENAME_SIZE, "temp____%s", FILE_SCORE);
 
     // open the original file for reading, and the temp file for writing
     FileData file = readFile(FILE_SCORE);
     FileData temp = writeFile(temp_filename);
 
-    do
+    // keep track of the current line number we are reading from the file
+    int current_line = 1;
+
+    // loop through each line in the file
+    while (fgets(file.file_row, MAX_LINE, file.file_ptr))
     {
-        // read the next line of the file into the buffer
-        fgets(file.file_row, MAX_LINE, file.file_ptr);
-        // if we've reached the end of the file, stop reading
+        // if we've reached the end of the file or the max number of lines, stop reading
         if (feof(file.file_ptr) || current_line == 5)
         {
-            // Scenario: empty row - fprintf() allows to write new line on same row
+            // scenario: empty row - fprintf() allows to write new line on same row
             if (current_line == write_line)
                 fprintf(temp.file_ptr, "%s\n", newline);
-
             // stop reading
-            keep_reading = false;
+            break;
         }
 
         // scenario: found row - fprintf() allows to write new line on same row
-        else if (current_line == write_line)
-            fprintf(temp.file_ptr, "%s\n%s%s", newline, file.file_row, "\n");
-
+        if (current_line == write_line)
+            fprintf(temp.file_ptr, "%s\n%s", newline, file.file_row);
         // scenario: same row
         else
             fputs(file.file_row, temp.file_ptr);
 
         // increment the current line as we will now be reading the next line
         current_line++;
-
-    } while (keep_reading);
+    }
 
     // close our access to both files as we are done with them
     fclose(file.file_ptr);
