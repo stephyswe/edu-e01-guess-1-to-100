2d1714a45de42ea5b146001bdff19b8d1f8be405
Stephanie <stephaniiehallberg@gmail.com>
2023-02-22 13:55:24 +0100

    feat(define): syntax cleanup

--- Define.h
+++ Define.h
@@ -1,28 +1,21 @@
 // All defined variables are stored here
 
-// file.c
-// define constants
+// file
+#define FILE_SCORE "scoreboard.txt"
 #define FILENAME_SIZE 1024
 #define MAX_LINE 2048
 #define MAX_LINES 5
 
-// file.h
-#define FILE_SCORE "scoreboard.txt"
-
-// input.h
-// Define variables
+// input
 #define MAX_SCORES 6
 #define MAX_NAME 20
 
-//main.c
-// Define options
+//main
 #define OPTION_PLAY_AGAIN 1
 #define OPTION_EXIT 2
 #define OPTION_VIEW_SCOREBOARD 3
 
-//prompt.c
+//prompt
 #define PROMPT_MIN 1
-
-//prompt.h
 #define GAME_MAX 100
 #define MENU_MAX 3
